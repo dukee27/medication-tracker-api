6c74080f733e8ae492336c2cb3b59990677e67be
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2026-01-17 00:04:16 +0530

    some changes

--- src/main/java/com/halt/medtracker/medication_tracker_api/domain/medication/MedicationSchedule.java
+++ src/main/java/com/halt/medtracker/medication_tracker_api/domain/medication/MedicationSchedule.java
@@ -1,6 +1,7 @@
 package com.halt.medtracker.medication_tracker_api.domain.medication;
 
 import com.halt.medtracker.medication_tracker_api.constants.FrequencyType;
+import com.halt.medtracker.medication_tracker_api.constants.IntakeTiming;
 import com.halt.medtracker.medication_tracker_api.domain.base.BaseEntity;
 
 import jakarta.persistence.*;
@@ -10,19 +11,30 @@ import java.time.LocalTime;
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
+@Builder
 public class MedicationSchedule extends BaseEntity {
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "medication_id", nullable = false)
     private Medication medication;
 
     @Enumerated(EnumType.STRING)
-    private FrequencyType frequency;
+    private FrequencyType frequencyType;
 
-    private LocalTime scheduleTime; 
+    // at what time of day
+    private LocalTime time;
+
     private Integer dayOfWeek;
+
+    // for every nth day
+    private Integer intervalDays;
+
+    // multiple doses per day
+    private Integer timesPerDay;
+
+    // before-after meal
+    @Enumerated(EnumType.STRING)
+    private IntakeTiming intakeTiming;
 }
