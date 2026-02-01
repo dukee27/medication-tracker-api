2b98c137dbdc7ba25cf68b048d8a515ca925c9c9
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2026-02-01 16:04:17 +0530

    adding mapper for intake time

--- src/main/java/com/halt/medtracker/medication_tracker_api/dto/mapper/MedicationIntakeTimeMapper.java
+++ src/main/java/com/halt/medtracker/medication_tracker_api/dto/mapper/MedicationIntakeTimeMapper.java
@@ -1,5 +1,18 @@
 package com.halt.medtracker.medication_tracker_api.dto.mapper;
 
+import org.springframework.stereotype.Component;
+
+import com.halt.medtracker.medication_tracker_api.domain.medication.MedicationIntakeTime;
+import com.halt.medtracker.medication_tracker_api.dto.response.MedicationIntakeTimeResponseDTO;
+
+@Component
 public class MedicationIntakeTimeMapper {
 
+    public MedicationIntakeTimeResponseDTO toResponse(MedicationIntakeTime intakeTime) {
+        return MedicationIntakeTimeResponseDTO.builder()
+                .intakeTimeId(intakeTime.getId())
+                .scheduleId(intakeTime.getSchedule().getId())
+                .intakeTime(intakeTime.getIntakeTime())
+                .build();
+    }
 }
